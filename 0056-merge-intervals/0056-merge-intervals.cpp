460fb4b6f213daec4083c3a59ba018876241b318
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-08-17 17:43:42 +0530

    Time: 26 ms (94.44%), Space: 19 MB (44.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -28,9 +28,8 @@ public:
             if(ans.empty()) ans.push_back(intervals[i]);
             else{
                 vector<int>&temp=ans.back();
-                int end=temp[1];
-                if(intervals[i][0] <= end){
-                    temp[1]=max(end,intervals[i][1]);
+                if(intervals[i][0] <= temp[1]){
+                    temp[1]=max(temp[1],intervals[i][1]);
                 }
                 else{
                     ans.push_back(intervals[i]);
