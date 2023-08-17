dd5ee521b9fccac2fabbf515e60e6acea39d6336
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-08-17 17:41:05 +0530

    Time: 28 ms (90.5%), Space: 19.1 MB (44.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,24 +7,36 @@ public:
 
         sort(intervals.begin(),intervals.end());
 
-        for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
+        // for(int i=0;i<n;i++){
+        //     int start=intervals[i][0];
+        //     int end=intervals[i][1];
 
-            if(!ans.empty() && end <= ans.back()[1]) continue;
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0] <= end){
-                    end=max(end,intervals[j][1]);
+        //     if(!ans.empty() && end <= ans.back()[1]) continue;
+        //     for(int j=i+1;j<n;j++){
+        //         if(intervals[j][0] <= end){
+        //             end=max(end,intervals[j][1]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back({start,end});
+
+        // }
+
+        for(int i=0;i<n;i++){
+            if(ans.empty()) ans.push_back(intervals[i]);
+            else{
+                vector<int>&temp=ans.back();
+                int end=temp[1];
+                if(intervals[i][0] <= end){
+                    temp[1]=max(end,intervals[i][1]);
                 }
                 else{
-                    break;
+                    ans.push_back(intervals[i]);
                 }
             }
-            ans.push_back({start,end});
-
         }
-
-
         return ans;
 
     }
