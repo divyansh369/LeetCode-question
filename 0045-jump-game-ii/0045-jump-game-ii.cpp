35d79d0ddc7143c4c642609f6f649836abe65ad5
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-09-06 21:06:51 +0530

    Time: 12 ms (75.67%), Space: 16.5 MB (95.31%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        queue<int>q;
-        q.push(0);int jump=0;
-        unordered_set<int>vis = {0};
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                int ind=q.front();q.pop();
-                if(ind >= nums.size()-1) return jump;
-                for(int i=1;i<=nums[ind];i++){
-                    int newInd= i + ind;
-                    if(vis.count(newInd) ) continue;
-                    vis.insert(newInd);
-                    q.push(newInd);
-                }
+        int curr=0;int farthest=0;int jump=0;
+        for(int i=0;i<nums.size()-1;i++){
+            farthest=max(farthest, i + nums[i]);
+            if(curr==i){
+                curr=farthest;
+                jump++;
             }
-            jump++;
         }
-        return 0;
+        return jump;
     }
 };
