105cea2e3c7796ca108b8028baa016e611fcb822
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-09-07 22:17:16 +0530

    Time: 0 ms (100%), Space: 9 MB (46.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,19 +5,22 @@ public:
         int j=n-1;
         int k=n+m-1;
 
-        while(j>=0 && i>=0){
+        while(i>=0 && j>=0){
             if(nums1[i] > nums2[j]){
                 nums1[k]=nums1[i];
                 i--;k--;
-            }
-            else{
+            }else{
                 nums1[k]=nums2[j];
                 j--;k--;
             }
         }
-        while(j>=0){
+        while(j >=0){
             nums1[k]=nums2[j];
             j--;k--;
+        }
+        while(i >=0){
+            nums1[k]=nums1[i];
+            i--;k--;
         }
 
     }
