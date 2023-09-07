0355771386226eada3df264290a8349e83cc0866
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-09-07 23:34:17 +0530

    Time: 26 ms (87.52%), Space: 18.9 MB (73.62%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,18 +6,16 @@ public:
         sort(intervals.begin(),intervals.end());
 
         for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            
-            if(!ans.empty() && end <= ans.back()[1]) continue;
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0] <= end){
-                    end = max(intervals[j][1],end);
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                if(ans.back()[1] >= intervals[i][0]){
+                    ans.back()[1] = max(intervals[i][1],ans.back()[1]);
                 }
             }
-
-            ans.push_back({start,end});
         }
         return ans;
+
     }
 };
