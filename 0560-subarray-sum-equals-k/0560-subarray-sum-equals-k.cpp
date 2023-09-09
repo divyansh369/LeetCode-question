3bfd19c166a3353d280e23077676f9e65f636595
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-09-09 20:24:51 +0530

    Time: 73 ms (67.75%), Space: 42.6 MB (55.9%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,15 +4,19 @@ public:
         int n=arr.size();int cnt=0;
         unordered_map<int,int>prevSum;
 
+        vector<int>prefix(n);
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix[i] = arr[i] + prefix[i-1];
+        }
         int sum=0;
         for(int i=0;i<n;i++){
-            sum += arr[i];
-            if(sum == k ) cnt++;
-            //check whether sum - k present in map
-            if(prevSum.find(sum - k) != prevSum.end()){
-                cnt += prevSum[sum-k];
-            } 
-            prevSum[sum]++;
+
+            if(prefix[i]==k) cnt++;
+            if(prevSum.find(prefix[i] - k) != prevSum.end()){
+                cnt += prevSum[prefix[i] - k];
+            }
+            prevSum[prefix[i]]++;
         }
         return cnt;
     }
