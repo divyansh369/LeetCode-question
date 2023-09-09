694e7e80c09b143dd53dc8b2b1e073d18eec18dd
Divyansh Srivastava <71817815+divyansh369@users.noreply.github.com>
2023-09-09 22:23:54 +0530

    Time: 10 ms (83.24%), Space: 16.6 MB (45.6%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int curr=0;int farthest=0;int jump=0;
-        for(int i=0;i<nums.size()-1;i++){
-            farthest=max(farthest, i + nums[i]);
-            if(curr==i){
-                curr=farthest;
+        int currReach=0,maxReach=0,jump=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            maxReach = max(maxReach,i+nums[i]);
+            if(maxReach >= nums.size()-1){
                 jump++;
+                break;
+            }
+            if(i==currReach){
+                jump++;
+                currReach=maxReach;
             }
         }
         return jump;
+
     }
 };
